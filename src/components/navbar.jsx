d4726d341ae7aa05a72fbe3408855ca419d0a737
deshinta <deshintaaulia065@gmail.com>
2024-07-03 19:12:45 +0700

    feat: add navbar component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Navbar({ page }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex justify-between items-center bg-[#7d7d7d] py-3 px-16">
+        <div className="flex items-center">
+          <img
+            className="rounded-full w-12 mr-3"
+            src="./voucher.jpg"
+            alt="logo_voucher"
+          />
+          <h2 className="text-lg font-bold">Voucher</h2>
+        </div>
+        <div>
+          <h1 className="text-lg font-bold">Judul</h1>
+        </div>
+        <div>
+          <button
+            className="py-2 px-9 rounded border border-black hover:bg-black hover:text-white"
+            onClick={() =>
+              page === "Home" ? navigate("/") : navigate("myVoucher")
+            }
+          >
+            {page}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import Navbar from "../components/navbar";
+
 export default function Home() {
-  return <h1 className="text-center">Page Home</h1>;
+  return (
+    <>
+      <Navbar page="history" />
+      <h1 className="text-center">Page Home</h1>
+    </>
+  );
 }
